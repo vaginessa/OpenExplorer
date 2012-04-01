49556e30c7b8a907ed4522dc07a5b74e2782b853
Brandon Bowles (Sony7) <BrandonBowles@gmail.com>
2012-04-01 01:53:57 -0700

    Editor

--- src/org/brandroid/openmanager/activities/EditorActivity.java
+++ src/org/brandroid/openmanager/activities/EditorActivity.java
@@ -1,18 +1,45 @@
 package org.brandroid.openmanager.activities;
 
-import android.content.Intent;
-import android.os.Bundle;
+import java.io.IOException;
 
-public class EditorActivity extends OpenExplorer
+import org.brandroid.openmanager.R;
+import org.brandroid.openmanager.data.OpenFile;
+import org.brandroid.openmanager.data.OpenPath;
+import org.brandroid.openmanager.fragments.TextEditorFragment;
+import org.brandroid.openmanager.util.FileManager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+public class EditorActivity extends OpenFragmentActivity
 {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		Intent intent = getIntent();
-		intent.setAction(Intent.ACTION_EDIT);
-		intent.addCategory(Intent.CATEGORY_DEFAULT);
-		intent.setData(intent.getData());
-		setIntent(intent);
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.text_editor_fragment);
+		
+		OpenPath file = null;
+		try {
+			file = FileManager.getOpenCache(getIntent().getDataString(), false, null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if(file == null)
+		{
+			Toast.makeText(this, R.string.s_error_no_intents, Toast.LENGTH_LONG);
+			finish();
+		}
+		Fragment mContentFragment = new TextEditorFragment(file);
+		getSupportFragmentManager().beginTransaction()
+			.replace(android.R.id.widget_frame, mContentFragment)
+			.commit();
 	}
 }
