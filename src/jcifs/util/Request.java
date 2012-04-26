e02c86ec9d51d4625877487644800c46d3f995c1
Brandon Bowles (Brandon5) <BrandonBowles@gmail.com>
2012-04-26 14:41:51 -0700

    jcifs tidying

---
+++ src/jcifs/util/Request.java
@@ -0,0 +1,4 @@
+package jcifs.util;
+
+public interface Request {
+}
---
+++ src/jcifs/util/Response.java
@@ -0,0 +1,6 @@
+package jcifs.util;
+
+public abstract class Response {
+    public long expiration;
+    public boolean isReceived;
+}
