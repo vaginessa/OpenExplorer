afc90b98a1eb63bde33e25f33e8fdccd79cf3972
Brandon Bowles (Brandon5) <BrandonBowles@gmail.com>
2012-07-27 10:52:08 -0700

    Cleaned up Preferences
    Fixed bug for global preferences

--- src/org/brandroid/utils/Preferences.java
+++ src/org/brandroid/utils/Preferences.java
@@ -67,6 +67,7 @@ public class Preferences {
 	}
 	public static SharedPreferences getPreferences(String file)
 	{
+		if(file == null) file = "global";
 		return getPreferences(mContext, file);
 	}
 	
